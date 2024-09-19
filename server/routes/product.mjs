5c2aeda5102f237b71b17d5923fa8cd7621e7f7d
solitdio079 <solitdio079@gmail.com>
2024-09-19 19:28:27 +0300

    search

--- server/routes/product.mjs
+++ server/routes/product.mjs
@@ -23,14 +23,22 @@ const pickType = (type) => {
 
 
 router.get("/", async (req, res) => {
-    const { cursor, limit, type } = req.query
+    const { cursor, limit, type, price } = req.query
+   // console.log(price)
     const query = {}
+    let filter = null
     if (cursor) {
         query._id = {$gt: cursor}
     }
-
+    if (price === 'asc') {
+      filter = { sort: { price: 1 } }
+    }
+    if (price === 'desc') {
+     filter = { sort: { price: -1 } }
+    }
+    console.log(filter);
     try {
-        const items = await pickType(type).find(query).limit(Number(limit))
+        const items = await pickType(type).find(query,null,filter).limit(Number(limit))
         return res.send(items)
         
     } catch (error) {
@@ -38,20 +46,76 @@ router.get("/", async (req, res) => {
     }
 })
 
+// get by Search terms
+
+router.get("/search", async (req, res) => {
+    const { q,cursor, limit, price } = req.query
+    ///console.log(price)
+    const query = { name: { $regex: new RegExp(`${q}`, 'i') } }
+    const filter = {limit: Number(limit)}
+    if (cursor) {
+        query._id = {$gt: cursor}
+    }
+     if (price === 'asc') {
+       filter.sort= { price: 1 } 
+     }
+     if (price === 'desc') {
+       filter.sort = { price: -1 } 
+     }
+    console.log(filter)
+    try {
+        const searchResults = await Product.find(query, null, filter)
+        return res.send(searchResults)
+        
+    } catch (error) {
+        return res.send({error: error.message})
+    }
+})
+
+// get Product by sales
+router.get('/sales/:salesId', async (req, res) => {
+  const {salesId} = req.params
+  const { cursor, limit, price } = req.query
+
+  const query = { 'onSale.sales_id': salesId }
+  const filter = { limit: Number(limit) }
+  if (cursor) {
+    query._id = { $gt: cursor }
+  }
+  if (price === 'asc') {
+    filter.sort = { price: 1 }
+  }
+  if (price === 'desc') {
+    filter.sort = { price: -1 }
+  }
+  try {
+    const salesProducts = await Product.find(query, null, filter)
+    return res.send(salesProducts)
+  } catch (error) {
+    return res.send({ error: error.message })
+  }
+})
 
 // get all product from categories
 
 router.get("/:categoryId", async (req, res) => {
     const { categoryId } = req.params
     //console.log(categoryId)
-    const { cursor, limit } = req.query
+    const { cursor, limit, price } = req.query
     console.log(cursor)
-    const query = {'category.category_id': categoryId}
+    const query = { 'category.category_id': categoryId }
+    let filter = null
     if (cursor) {
         query._id = {$gt: cursor}
     }
+    if (price === "asc") {
+        filter ={ sort :{price: 1} }
+    }
+    if (price === 'desc') {
+        filter = {sort: {price: -1 }}
+    }
     try {
-        const products = await Product.find(query).limit(Number(limit)) 
+        const products = await Product.find(query,null, filter).limit(Number(limit)) 
         //console.log(products)
         return res.send(products)
         

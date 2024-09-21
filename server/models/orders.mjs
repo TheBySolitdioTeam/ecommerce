662425f33163f453431f9d4762eea1b1466a10fc
solitdio079 <solitdio079@gmail.com>
2024-09-21 21:31:03 +0300

    added cart

--- server/models/orders.mjs
+++ server/models/orders.mjs
@@ -15,32 +15,60 @@ const cartSubDoc = new Schema({
     required: [true, 'The subtotal is required!']
   },
 })
+const addressSubDoc = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please give that address a name'],
+  },
+  addressLines: {
+    type: String,
+    required: [true, 'Please enter the address lines'],
+  },
+  city: {
+    type: String,
+    required: [true, 'Please enter the city!'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Please enter the country!'],
+  },
+  zipCode: {
+    type: String,
+    required: [true, 'Please enter the zipcode!'],
+  },
+  user_id: {
+    type: mongoose.Types.ObjectId,
+    required: [true, 'Please provide a user for this address!'],
+  },
+})
 
 
 
 
 
 const orderSchema = new Schema({
-    cart: {
-        type: cartSubDoc,
-        required: [true, 'Please provide a cart!']
-        
-     },
-    total: {
-        type: mongoose.Decimal128,
-        required: [true, 'Please provide a total price for the order!']
-    },
-    status: {
-        type: String,
-        default: 'Order Received'
-    },
-    user: userSubDoc,
-    payment: mongoose.Mixed,
-    address: {
-        type: mongoose.Types.ObjectId,
-        required: [true, 'Please provide an address']
-    }
-
+  cart: {
+    type: cartSubDoc,
+    required: [true, 'Please provide a cart!'],
+  },
+  total: {
+    type: mongoose.Decimal128,
+    required: [true, 'Please provide a total price for the order!'],
+  },
+  status: {
+    type: String,
+    default: 'Order Received',
+  },
+  user: userSubDoc,
+  payment: mongoose.Mixed,
+  address: {
+    type: addressSubDoc,
+    required: [true, 'Please provide an address'],
+  },
+  shipping: {
+    type: String,
+    default: 'Company',
+  },
 })
 
 

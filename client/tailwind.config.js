9158c8ef36e0626bc93711d9d9ce65a7fd2505d5
solitdio079 <solitdio079@gmail.com>
2024-10-04 01:15:03 +0300

    logo and themes changed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#354b82',
+          secondary: '#d29f72',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+      'dark',
+      'cupcake',
+    ],
+  },
   plugins: [daisyui],
 }
 

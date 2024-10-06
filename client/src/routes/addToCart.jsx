95bf8e3dcef1be700799755f13cc18de1580b7cd
solitdio079 <solitdio079@gmail.com>
2024-10-06 12:34:17 +0300

    solved the cart situation 5

--- client/src/routes/addToCart.jsx
+++ client/src/routes/addToCart.jsx
@@ -2,7 +2,7 @@ import {redirect} from 'react-router-dom'
 export async function action({ request }) {
     const formData = await request.formData()
     const bodyObj = Object.fromEntries(formData)
-
+    bodyObj.user_id= localStorage.getItem("user_id")
     try {
         const response = await fetch('https://api.mobilium.info/cart/', {
           method: 'POST',

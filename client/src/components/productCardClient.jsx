05d7286569f7cc792c9ac20b86cd4372713a34c5
solitdio079 <solitdio079@gmail.com>
2024-11-10 16:24:58 +0300

    product card changed

--- client/src/components/productCardClient.jsx
+++ client/src/components/productCardClient.jsx
@@ -7,48 +7,50 @@ export default function ProductCardClient({ item }) {
   const location = useLocation()
   const fetcher= useFetcher()
   return (
-    <div className="card m-3  bg-base-100 w-60 lg:w-72 shadow-xl">
+    <div className="card m-3 image-full  bg-base-100 w-60 lg:w-72 shadow-xl">
       <figure>
         <Link to={`/singleProduct/${item._id}`}>
           <img
             className="max-h-80"
-            src={
-              'https://api.mobilium.info/' + item.images.split(';')[0]
-            }
+            src={'https://api.mobilium.info/' + item.images.split(';')[0]}
             alt="Shoes"
           />
         </Link>
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          {item.name}
-          <div className="badge badge-secondary">{item.type}</div>
-        </h2>
+        <Link to={`/singleProduct/${item._id}`}>
+          <h2 className="card-title">
+            {item.name}
+            <div className="badge badge-secondary">{item.type}</div>
+          </h2>
+        </Link>
 
         <div className="card-actions flex flex-col justify-end">
-          <p> {item.description} </p>
-          <div>
-            <span
-              className={item.onSale ? 'line-through' : 'text-xl font-bold'}
-            >
-              ${item.price.$numberDecimal}
-            </span>
-            {item.onSale ? (
-              <span className="text-xl font-bold mx-2">
-                $
-                {(
-                  item.price.$numberDecimal -
-                  (item.onSale.discount_rate.$numberDecimal / 100) *
-                    item.price.$numberDecimal
-                ).toFixed(2)}
+          <Link to={`/singleProduct/${item._id}`}>
+            <p> {item.description} </p>
+            <div>
+              <span
+                className={item.onSale ? 'line-through' : 'text-xl font-bold'}
+              >
+                ${item.price.$numberDecimal}
               </span>
-            ) : (
-              ''
-            )}
-          </div>
-          <div className="flex flex-row w-full justify-end my-3">
-            <div className="badge badge-outline"> {item.category.name} </div>
-          </div>
+              {item.onSale ? (
+                <span className="text-xl font-bold mx-2">
+                  $
+                  {(
+                    item.price.$numberDecimal -
+                    (item.onSale.discount_rate.$numberDecimal / 100) *
+                      item.price.$numberDecimal
+                  ).toFixed(2)}
+                </span>
+              ) : (
+                ''
+              )}
+            </div>
+            <div className="flex flex-row w-full justify-end my-3">
+              <div className="badge badge-outline"> {item.category.name} </div>
+            </div>
+          </Link>
           <fetcher.Form
             method="post"
             action="/product/addToCart"

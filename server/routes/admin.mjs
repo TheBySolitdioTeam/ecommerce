5cc2d2c4c82978320949013bc185e254dd65ada1
solitdio079 <solitdio079@gmail.com>
2024-11-12 06:08:03 +0300

    added content to admin

--- server/routes/admin.mjs
+++ server/routes/admin.mjs
@@ -2,6 +2,7 @@ import express,{ Router } from "express";
 import categoryRouter from './admin/category.mjs'
 import productRouter from './admin/product.mjs'
 import salesRouter from './admin/sales.mjs'
+import contentRouter from './admin/content.mjs'
 
 
 const router = Router()
@@ -14,5 +15,6 @@ router.use(checkAdmin)
 router.use('/category', categoryRouter)
 router.use('/product', productRouter)
 router.use('/sales', salesRouter)
+router.use('/content', contentRouter)
 
 export default router

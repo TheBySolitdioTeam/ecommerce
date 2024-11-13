de8646528403f75b08ae06b139e6ec73657dd620
solitdio079 <solitdio079@gmail.com>
2024-11-13 11:56:55 +0300

    navbar single

--- client/src/routes/content/singleContent.jsx
+++ client/src/routes/content/singleContent.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/autoplay'
 import 'swiper/css/scrollbar'
 import 'swiper/css/mousewheel'
 import { Pagination, Autoplay, Navigation } from 'swiper/modules'
+import AnimatedLayout from "../../animation/animatedLayout"
 // Getting the single content
 export async function loader({ params }) {
   const { id } = params
@@ -36,30 +37,35 @@ export async function loader({ params }) {
 export default function SingleContent() {
     const singleContent = useLoaderData()
     return (
-      <div className="flex flex-col justify-center">
-        <Swiper
-          modules={[Pagination, Navigation, Autoplay]}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-          }}
-          navigation
-          loop={true}
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          spaceBetween={50}
-          className="w-full max-h-screen"
-        >
-          {singleContent.images.map((item) => (
-            <SwiperSlide key={item}>
-              <img src={'https://api.mobilium.info/' + item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className="p-10">
-          <h1 className="text-3xl my-3 border-b-2 border-primary font-bold lg:text-5xl"> {singleContent.name} </h1>
-          <p className=""> {singleContent.details} </p>
+      <AnimatedLayout>
+        <div className="flex flex-col justify-center">
+          <Swiper
+            modules={[Pagination, Navigation, Autoplay]}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            navigation
+            loop={true}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            spaceBetween={50}
+            className="w-full max-h-screen"
+          >
+            {singleContent.images.map((item) => (
+              <SwiperSlide key={item}>
+                <img src={'https://api.mobilium.info/' + item} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="p-10">
+            <h1 className="text-3xl my-3 border-b-2 border-primary font-bold lg:text-5xl">
+              {' '}
+              {singleContent.name}{' '}
+            </h1>
+            <p className=""> {singleContent.details} </p>
+          </div>
         </div>
-      </div>
+      </AnimatedLayout>
     )
 }

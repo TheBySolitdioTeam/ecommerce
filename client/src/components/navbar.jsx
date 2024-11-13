cfa98937243eb2ab1df39748f28280fd8a40e68f
solitdio079 <solitdio079@gmail.com>
2024-11-13 09:24:22 +0300

    added drawer 3

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -11,13 +11,13 @@ export default function Navbar({ user, cart }) {
     <>
       <div className="navbar backdrop-blur-sm sticky top-0 z-40 lg:z-50">
         <div className="navbar-start">
-          <div className="drawer lg:hidden">
+          <div className="drawer">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
               {/* Page content here */}
               <FaBars
                 htmlFor="my-drawer"
-                className="btn btn-primary drawer-button h-5 w-5"
+                className="drawer-button h-5 w-5 lg:hidden"
               />
             </div>
             <div className="drawer-side">

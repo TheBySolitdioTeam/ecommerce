549056c33c8a1330aec84802395f87407f831652
solitdio079 <solitdio079@gmail.com>
2025-04-10 00:06:34 +0300

    Add Test link for localhost

--- server/steategies/magic-link.mjs
+++ server/steategies/magic-link.mjs
@@ -26,7 +26,9 @@ passport.use(
       verifyUserAfterToken: true,
     },
     function send(user, token) {
-       var link = 'https://api.mobilium.info/auth/login/email/verify?token=' + token
+      var link =
+        "http://localhost:5173/auth/login/email/verify?token=' + token"
+       /*var link = 'https://api.mobilium.info/auth/login/email/verify?token=' + token*/
        sendSmtpEmail.subject = 'Login to Mobilium!'
        sendSmtpEmail.htmlContent =
          '<h3>Hello!</h3><p>Cliquez sur le lien ci-dessous pour vous connecter sur Mobilium.</p><p><a href="' +
